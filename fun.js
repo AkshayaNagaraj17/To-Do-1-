9e4f43f098d266cbee9504489a51a81bb99582a2
AkshayaNagaraj17 <cnagaraj332@gmail.com>
2024-08-20 20:50:11 +0530

    feat:final

--- fun.js
+++ fun.js
@@ -1,39 +1,31 @@
-let button = document.getElementById('add')
-let todoList = document.getElementById('todoList')
-let input = document.getElementById('input');
-//local storage,cookies
-let todos = [];
-window.onload = ()=>{
-    todos = JSON.parse(localStorage.getItem('todos')) || []
-    todos.forEach(todo=>addtodo(todo))
-}
+let btn=document.getElementById("add");
+let input=document.getElementById("input");
+let todoList=document.getElementById("todoList");
 
-button.addEventListener('click',()=>{
+let todos=[];
+
+btn.addEventListener("click",()=>
+{
     todos.push(input.value)
-    localStorage.setItem('todos',JSON.stringify(todos))
     addtodo(input.value)
-    input.value=''
+    input.value=""
 })
-
-function addtodo(todo){
-    let para = document.createElement('p');
-    para.innerText = todo;
-    todoList.appendChild(para)
-    
-    para.addEventListener('click',()=>{
-        para.style.textDecoration = 'line-through'
+function addtodo(todo)
+{
+    let par=document.createElement("p");
+    par.innerHTML=todo;
+    todoList.appendChild(par)
+    par.addEventListener("click",()=>{
+        par.style.textDecoration="line-through"
         remove(todo)
     })
-    para.addEventListener('dblclick',()=>{
-        todoList.removeChild(para)
-        remove(todo)
+    par.addEventListener("dblclick",()=>{
+        todoList.remove(par)
     })
 }
-
-function remove(todo){
-    let index = todos.indexOf(todo)
-    if (index > -1) {
-        todos.splice(index, 1);
-      }
-    localStorage.setItem('todos',JSON.stringify(todos))
+function remove(todo)
+{
+    let index=todos.index(todo)
+    if(index>-1)
+        todos.splice(index,1)
 }
