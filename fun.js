1d87ee9b7724040a6ef910057e95d029fcb3cc54
AkshayaNagaraj17 <cnagaraj332@gmail.com>
2024-08-20 20:51:18 +0530

    feat:final1

--- fun.js
+++ fun.js
@@ -1,31 +1,41 @@
 let btn=document.getElementById("add");
 let input=document.getElementById("input");
 let todoList=document.getElementById("todoList");
-
+//local storage
 let todos=[];
+window.onload=()=>
+{
+   todos=JSON.parse(localStorage.getItem("todos")) || []
+    todos.forEach(todo=>addtodo(todo))
+}
 
 btn.addEventListener("click",()=>
 {
     todos.push(input.value)
+    localStorage.setItem("todos",JSON.stringify(todos))
     addtodo(input.value)
     input.value=""
 })
 function addtodo(todo)
 {
     let par=document.createElement("p");
-    par.innerHTML=todo;
+    par.innerText=todo;
     todoList.appendChild(par)
+    
     par.addEventListener("click",()=>{
         par.style.textDecoration="line-through"
         remove(todo)
     })
     par.addEventListener("dblclick",()=>{
-        todoList.remove(par)
+        todoList.removeChild(par)
+        remove(todo)
     })
 }
 function remove(todo)
 {
     let index=todos.index(todo)
-    if(index>-1)
-        todos.splice(index,1)
+    if(index> -1){
+        todos.splice(index,1);
+    }
+    localStorage.setItem("todos",JSON.stringify(todos))
 }
